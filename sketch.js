ca045c4bcd821e715b11cb759bf7982d89c920e9
flowmaster20 <31287387+flowmaster20@users.noreply.github.com>
2018-03-31 00:13:37 +0200

    adding

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 var y = 100;
-
+var i = 1;
 function setup() {
   createCanvas(150, 200);
   stroke(0);
@@ -10,9 +10,13 @@ function draw() {
   background(255);
   translate(width/2,height/2);
   stroke(0);
-
-  rotate(PI / random(0,10));
+i = i + 0.1;
+if(i>4){
+  i = 0.5;
+}
+  rotate(PI / i);
   line(0,0,30,30);
+
   noFill();
 ellipse(0,0,100);
 }
